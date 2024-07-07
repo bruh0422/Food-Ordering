a0e87a9f9e399c558a97c38e0ce056df5de211a8
bruh0422 <happyh960422@gmail.com>
2024-07-07 12:09:34 +0800

    新增資料審核

--- src/data_handler.py
+++ src/data_handler.py
@@ -14,3 +14,28 @@ def parse_to_dict(parse) -> dict:
     data = {k: v[0] if len(v) == 1 else v for k, v in parse_qs(parse).items()}
 
     return data
+
+def data_check():
+    if not os.path.exists(os.path.join('data', 'system')):
+        os.mkdir(os.path.join('data', 'system'))
+
+    if not os.path.exists(os.path.join('data', 'system', 'nickname.json')):
+        write_data({}, 'system', 'nickname.json')
+    if not os.path.exists(os.path.join('data', 'system', 'order.json')):
+        write_data(
+        {
+            'finished': {
+                'accepted': {},
+                'rejected': {},
+            },
+            'pending': {}
+        },
+        'system', 'order.json')
+    if not os.path.exists(os.path.join('data', 'system', 'personnel.json')):
+        write_data(
+        {
+            'admin': [],
+            'staff': [],
+            'student': []
+        },
+        'system', 'personnel.json')
